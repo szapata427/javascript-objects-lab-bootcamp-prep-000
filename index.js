04fc2c8605337eb219f535896ace055152b50461
Steven Zapata <szapata427@gmail.com>
2018-08-08 13:36:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var recipes = {};
 
- function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key]: value});
-}
-
-function destructivelyUpdteObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  
-  return object
-}
 
 destructivelyUpdteObjectWithKeyAndValue(object, 'test', 'pass')
