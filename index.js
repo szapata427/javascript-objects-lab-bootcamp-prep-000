169c9727378a126524fb04d6214f2fee3eab5057
Steven Zapata <szapata427@gmail.com>
2018-08-08 13:33:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = {};
  function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
+
+function destructivelyUpdteObjectWithKeyAndValue(object, key, value) {
+  
+}
